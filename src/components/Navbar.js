6ba4cee9a2459039a595adb38768e816bed65406
Aditya Bheke <adityabheke34@gmail.com>
2024-10-22 10:42:10 +0530

    Create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+
+const Navbar = ()=>{
+    return <nav>
+        <a href="/" id="brand-info">
+            <img src="https://cdn-icons-png.flaticon.com/128/3845/3845723.png" alt="brand-logo"/>
+            <h1>PhotoFolio</h1>
+        </a>
+    </nav>
+}
+export default Navbar;
