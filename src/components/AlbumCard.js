915c384f4e406d1ca44fb3d153d2d8d22e3e32da
Aditya Bheke <adityabheke34@gmail.com>
2024-10-23 17:54:18 +0530

    change classname

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -2,7 +2,7 @@
 function AlbumCard(props) {
     const {cardInfo} = props;
     return <div className="album-card" onClick={()=>{props.handleSelect("imagesList", cardInfo)}}>
-        <div className="image-container">
+        <div className="album-thumbnail">
             <img src={"https://cdn-icons-png.flaticon.com/128/3342/3342137.png"} alt={cardInfo.title} />
         </div>
         <h3>{cardInfo.title}</h3>
